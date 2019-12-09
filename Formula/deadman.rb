2015a927692423ed2bfbe9ccda76d98922c7660c
Naoya Kaneko <enukane@glenda9.org>
2019-12-09 15:10:21 +0900

    fix deadman.rb
    Signed-off-by: Naoya Kaneko <enukane@glenda9.org>

--- Formula/deadman.rb
+++ Formula/deadman.rb
@@ -1,5 +1,6 @@
 class Deadman < Formula
-  desc ""
+  desc "curses based host status checking application using ping "
+  version "16.11.11"
   url "https://github.com/upa/deadman", :using => :git
   depends_on "python"
 
