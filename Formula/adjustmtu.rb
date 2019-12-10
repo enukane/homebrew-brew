442ffbc0b819f6bec68f0310460df7ce4eeafcdc
Naoya Kaneko <enukane@glenda9.org>
2019-12-10 11:05:24 +0900

    add adjustmtu
    Signed-off-by: Naoya Kaneko <enukane@glenda9.org>

---
+++ Formula/adjustmtu.rb
@@ -0,0 +1,10 @@
+class Adjustmtu < Formula
+  desc ""
+  version "0.1.0"
+  url "https://github.com/ryo/adjustmtu.git", :using => :git
+
+  def install
+    system "clang", "-o adjustmtu", "adjustmtu.c"
+    bin.install "adjustmtu"
+  end
+end[
